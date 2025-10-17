395c5344aa5d095d046e50aebc5e0951aa1e850e
若琳酱 <ruollin@outlook.com>
2025-10-17 20:41:55 +0800

    feat(network): 添加 WebUtils 用于网络请求

---
+++ MiraiCL.Core/Network/WebUtils.cs
@@ -0,0 +1,12 @@
+namespace MiraiCL.Core.Network;
+
+public static class WebUtils{
+    public static async Task<HttpResponseHandler> MakeRequestOnce(HttpRequestMessage request){
+        
+    }
+
+    public static async Task<string> GetStringRetry(){
+
+    }
+
+}
